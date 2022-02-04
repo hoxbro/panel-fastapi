8b90df0678dc1ed4044bdfb9bd7e667f7c4f6fd7
Simon Høxbro <simon.hansen@me.com>
2022-02-04 17:50:37 +0100

    Added sliders

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 from .button_click import ButtonClick
+from .sliders import SineWave
 
 # Fill out with new models
 _models = [
     #  [url, title, model]
     ["buttonclick", "Button Click", ButtonClick],
+    ["sinewave", "Sine Wave", SineWave],
 ]
 
 titles = {m[0]: m[1] for m in _models}
