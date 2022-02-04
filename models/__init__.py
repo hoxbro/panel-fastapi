36318ecdb31a19ee12dfcd69d3c534de899279e7
Simon Høxbro <simon.hansen@me.com>
2022-02-04 17:21:02 +0100

    Updated __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,10 @@
 from .button_click import ButtonClick
 
+# Fill out with new models
+_models = [
+    #  [url, title, model]
+    ["buttonclick", "Button Click", ButtonClick],
+]
 
-# Fill both out with new models
-titles = {"buttonclick": "Button Click"}
-serving = {"/panel/buttonclick": ButtonClick}
+titles = {m[0]: m[1] for m in _models}
+serving = {f"/panel/{m[0]}": m[2] for m in _models}
